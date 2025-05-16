f8a1809ada89a57f3b89fb18e551dd27066eab2d
pers-ephone <148533426+pers-ephone@users.noreply.github.com>
2025-05-16 15:07:32 +0200

    insert imports on first line

--- src/commands/InsertTemplateCommand.ts
+++ src/commands/InsertTemplateCommand.ts
@@ -9,18 +9,26 @@ export async function insertTemplate(template: Template) {
     if (!editor) { return; }
 
     const enableImports: boolean = config.shouldInsertImports;
-    const imports: string[] = template.imports || [];
-    const codeLines: string[] = [
-        ...(enableImports ? imports : []),
-        ...template.code
-    ];
 
-    const code: string = codeLines.join('\n');
+    const code: string = template.code.join('\n');
     const selection: Selection = editor.selection;
 
     await editor.edit(editBuilder => {
         editBuilder.insert(selection.active, code);
     });
+
+    const splitImports: string[] = template.imports || [];
+    if (enableImports && splitImports.length > 0 &&
+        !(splitImports.length === 1 && splitImports[0] === "")) {
+            const imports: string = splitImports.join('\n') + '\n';
+
+            await editor.edit(editBuilder => {
+                editBuilder.insert(
+                    new Position(0, 0),
+                    imports
+                );
+            });
+    }
 
     if (config.shouldPreserveSelection) {
         const newPosition: Position = selection.active.translate(0, code.length);
